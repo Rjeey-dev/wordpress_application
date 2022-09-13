d1a344b8e2f7017f185e8d07ecffb10668fe58cb
vabashyn <86234390+vabashyn@users.noreply.github.com>
2022-09-13 13:42:45 +0300

    created module clients

---
+++ wp-content/themes/wordpress_application/modules/module-clients.php
@@ -0,0 +1,24 @@
+<?php
+defined('ABSPATH') OR exit('No direct script access allowed');
+?>
+<section id="clients" class="clients section-bg">
+    <div class="container">
+        <div class="row no-gutters clients-wrap clearfix wow fadeInUp">
+            <?php
+            $infografica = get_sub_field('clients');
+            if ( $infografica ) {
+                foreach ($infografica as $item) {
+                    ?>
+                    <div class="col-lg-2 col-md-4 col-6">
+                        <div class="client-logo">
+                            <img src="<?php echo $item['image']; ?>" class="img-fluid" alt="" data-aos="flip-right" <?php echo $item['key']; ?>>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+</section>
+<?php
--- wp-content/themes/wordpress_application/page.php
+++ wp-content/themes/wordpress_application/page.php
@@ -3,6 +3,13 @@ defined('ABSPATH') OR exit('No direct script access allowed');
 get_header();
 the_post();
 $page_id =  get_the_ID();
-?>
 
-<?php get_footer();
+if ( have_rows('page_builder') ) {
+    while ( have_rows('page_builder') ) {
+        the_row();
+
+        get_template_part('modules/'.get_row_layout());
+    }
+}
+
+get_footer();
