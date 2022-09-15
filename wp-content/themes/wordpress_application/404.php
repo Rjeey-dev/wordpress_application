5fe7a611a69bd9c86b9e729266905f4adadc9436
vabashyn <86234390+vabashyn@users.noreply.github.com>
2022-09-15 15:18:54 +0300

    created_404_page

--- wp-content/themes/wordpress_application/404.php
+++ wp-content/themes/wordpress_application/404.php
@@ -1,3 +1,14 @@
 <?php defined('ABSPATH') OR exit('No direct script access allowed'); get_header(); ?>
-
+    <section class="error-section clouds clouds-top">
+        <div class="container">
+            <div class="error-block">
+                <div class="bg-gradient"></div>
+                <div class="box">
+                    <h1><?php the_field('404_title', 'option'); ?></h1>
+                    <p><?php the_field('404_text', 'option'); ?> <a href="<?php echo get_template_directory_uri(); ?>">главной страницы</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="wafe-sprite"></div>
+    </section>
 <?php get_footer();
