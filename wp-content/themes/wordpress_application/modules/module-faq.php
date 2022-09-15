6be5d5f09f5de72410d4021d7e7c7e7429be0579
vabashyn <86234390+vabashyn@users.noreply.github.com>
2022-09-15 16:00:18 +0300

    created_post_faq

--- wp-content/themes/wordpress_application/modules/module-faq.php
+++ wp-content/themes/wordpress_application/modules/module-faq.php
@@ -1,30 +1,42 @@
 <?php
 defined('ABSPATH') OR exit('No direct script access allowed');
 ?>
-<section id="faq" class="faq section-bg">
-    <div class="container">
-        <div class="section-title">
-            <h2 data-aos="fade-in"><?php the_sub_field('title'); ?></h2>
-            <p data-aos="fade-in"><?php the_sub_field('main_text'); ?></p>
-        </div>
-        <?php
-        $faq = get_sub_field('faq');
-        if ( $faq ) {
-            foreach ($faq as $item) {
-                ?>
-                <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up">
-                    <div class="col-lg-5">
-                        <i class="bx bx-help-circle"></i>
-                        <h4><?php echo $item['text']; ?></h4>
+<?php
+$query = [
+    'posts_per_page' => 10,
+    'post_type' => 'faq',
+    'post_status' => 'publish',
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'paged' => souvel_paged(),
+];
+$query = new WP_Query($query);
+if ( $query->have_posts() ) {
+    ?>
+    <section id="faq" class="faq section-bg">
+        <div class="container">
+            <div class="section-title">
+                <h2 data-aos="fade-in"><?php the_sub_field('title'); ?></h2>
+                <p data-aos="fade-in"><?php the_sub_field('main_text'); ?></p>
+            </div>
+            <?php
+            while ( $query->have_posts() ) {
+                $query->the_post();
+                $post_id = get_the_ID();
+                    ?>
+                    <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up">
+                        <div class="col-lg-5">
+                            <i class="bx bx-help-circle"></i>
+                            <h4><?php $titleQua = get_field('title'); echo $titleQua ?></h4>
+                        </div>
+                        <div class="col-lg-7">
+                            <p><?php $descriptionQua = get_field('description'); echo $descriptionQua ?></p>
+                        </div>
                     </div>
-                    <div class="col-lg-7">
-                        <p><?php echo $item['description']; ?></p>
-                    </div>
-                </div>
-                <?php
+                    <?php
             }
-        }
-        ?>
-    </div>
-</section>
+}
+?>
+        </div>
+    </section>
 <?php
